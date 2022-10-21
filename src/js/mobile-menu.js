07d330042b196d911c849274712874346d515870
Mark Sakhatskyi <mark.sahaxkiy@gmail.com>
2022-10-21 16:19:57 +0300

    bugFixMobileMenu

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -2,6 +2,10 @@
   const mobileMenu = document.querySelector('.js-menu-container');
   const openMenuBtn = document.querySelector('.js-open-menu');
   const closeMenuBtn = document.querySelector('.js-close-menu');
+  const menuLink1 = document.getElementById('link_home');
+  const menuLink2 = document.getElementById('link_about');
+  const menuLink3 = document.getElementById('link_products');
+  const menuLink4 = document.getElementById('link_contact');
 
   const toggleMenu = () => {
     const isMenuOpen =
@@ -9,20 +13,24 @@
     openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
     mobileMenu.classList.toggle('is-open');
 
-    const scrollLockMethod = !isMenuOpen
-      ? 'disableBodyScroll'
-      : 'enableBodyScroll';
-    bodyScrollLock[scrollLockMethod](document.body);
+    // const scrollLockMethod = !isMenuOpen
+    //   ? 'disableBodyScroll'
+    //   : 'enableBodyScroll';
+    // bodyScrollLock[scrollLockMethod](document.body);
   };
 
   openMenuBtn.addEventListener('click', toggleMenu);
   closeMenuBtn.addEventListener('click', toggleMenu);
+  menuLink1.addEventListener('click', toggleMenu);
+  menuLink2.addEventListener('click', toggleMenu);
+  menuLink3.addEventListener('click', toggleMenu);
+  menuLink4.addEventListener('click', toggleMenu);
 
   // Close the mobile menu on wider screens if the device orientation changes
   window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
     if (!e.matches) return;
     mobileMenu.classList.remove('is-open');
     openMenuBtn.setAttribute('aria-expanded', false);
-    bodyScrollLock.enableBodyScroll(document.body);
+    // bodyScrollLock.enableBodyScroll(document.body);
   });
 })();
